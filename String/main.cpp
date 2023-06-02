622ae48864464e1f59fe8e640a2944d166ca4290
User <User@DESKTOP-3GAUKFT>
2023-06-02 22:56:10 +0300

    Methods out of class

--- String/main.cpp
+++ String/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;
 
+//////////////////////////////////////////////////////////////////////////
+////////////// объявление класса - class declaration       //////////////
 class String;
 String operator+(const String& left, const String& right);
 class String
@@ -12,96 +17,130 @@ class String
 	int size; //размер строки в Байтах
 	char* str; //адрес строки в динамической памяти
 public:
-	int get_size()const
-	{
-		return size;
-	}
-	const char* get_str()const
-	{
-		return str;
-	}
-	char* get_str()
-	{
-		return str;
-	}
+	int get_size()const;
+	const char* get_str()const;
+	char* get_str();
 
 	//			Constructor
 
-	explicit String(int size = 80):size(size),  str(new char[size]{})
-	{
-		cout << "DefConstructor:\t" << this << endl;
-	}
-
-	String(const char str[]):size(strlen(str)+1) , str(new char[size]{}) // в первую очередь в конструкторе  
-	{
-		for (int i = 0; i < size; i++) this->str[i] = str[i];// заполняем память
-		cout << "Constructor:\t" << this << endl;
-	}
-
-	String(const String& other):String(other.str) // const можно проинициализировать только в заголовке
-	{
-		cout << "CopyConsrtuctor:" << this << endl;
-	}
-
-	String(String&& other):size(other.size),str(other.str) // вот такую инициализацтию в заголовке 
-	{
-		//Shallow copy:
-		//this->size = other.size;
-		//this->str = other.str;
-		other.size = 0;
-		other.str = nullptr;
-		cout << "MoveConstructor:\t" << this << endl;
-	}
-
-
-	~String()
-	{
-		delete[] str;
-		cout << "Destructor" << this << endl;
-	}
+	explicit String(int size = 80);
+	String(const char str[]);
+	String(const String& other);
+	String(String&& other);
+	~String();
 
 	// operators
 
-	String& operator=(const String& other)
-	{
-		if (this == &other)return*this; // защита класса и объектов от нежданчика
-		delete[] str;
-		this->size = other.size;
-		this->str = new char[size] {};
-		for (int i = 0; i < size; i++)
-		{
-			this->str[i] = other.str[i];
-		}
-		cout << "CopyAssignment:\t" << this << endl;
-		return *this;
-	}
+	String& operator=(const String& other);
 
-	String& operator+=(const String& other)
-	{
-		return *this = *this + other;
-	}
+	String& operator+=(const String& other);
 
 
 
-	char operator[](int i)const
-	{
-		return str[i];
-	}
-	char& operator[](int i)
-	{
-		return str[i];
-	}
+	char operator[](int i)const;
+	char& operator[](int i);
 	
 	//				Methods:
 
-	void print()const
-	{
-		cout << "Size: " << size << endl;
-		cout << "Str:  " << str << endl;
-	}
+	void print()const;
 };
 
+//////////////////////////////////////////////////////////////////////////
+////////////// конец объявления класса - class declaration end      //////////////
 
+
+
+//////////////////////////////////////////////////////////////////////////
+////////////// ОПРЕДЕЛНИЕ КЛАССА - CLASS DEFINITION     //////////////
+
+// двойное двоеточие :: - это оператор разрешения видимости Scope Operator
+int String::get_size()const
+{
+	return size;
+}
+const char* String::get_str()const
+{
+	return str;
+}
+char* String::get_str()
+{
+	return str;
+}
+
+//			Constructor
+
+String::String(int size) :size(size), str(new char[size] {})
+{
+	cout << "DefConstructor:\t" << this << endl;
+}
+
+String::String(const char str[]) :size(strlen(str) + 1), str(new char[size] {}) // в первую очередь в конструкторе  
+{
+	for (int i = 0; i < size; i++) this->str[i] = str[i];// заполняем память
+	cout << "Constructor:\t" << this << endl;
+}
+
+String::String(const String& other) :String(other.str) // const можно проинициализировать только в заголовке
+{
+	cout << "CopyConsrtuctor:" << this << endl;
+}
+
+String::String(String&& other) :size(other.size), str(other.str) // вот такую инициализацтию в заголовке 
+{
+	//Shallow copy:
+	//this->size = other.size;
+	//this->str = other.str;
+	other.size = 0;
+	other.str = nullptr;
+	cout << "MoveConstructor:\t" << this << endl;
+}
+
+
+String::~String()
+{
+	delete[] str;
+	cout << "Destructor" << this << endl;
+}
+
+// operators
+
+String& String::operator=(const String& other)
+{
+	if (this == &other)return*this; // защита класса и объектов от нежданчика
+	delete[] str;
+	this->size = other.size;
+	this->str = new char[size] {};
+	for (int i = 0; i < size; i++)
+	{
+		this->str[i] = other.str[i];
+	}
+	cout << "CopyAssignment:\t" << this << endl;
+	return *this;
+}
+
+String& String::operator+=(const String& other)
+{
+	return *this = *this + other;
+}
+
+
+
+char String::operator[](int i)const
+{
+	return str[i];
+}
+char& String::operator[](int i)
+{
+	return str[i];
+}
+
+//				Methods:
+
+void String::print()const
+{
+	cout << "Size: " << size << endl;
+	cout << "Str:  " << str << endl;
+}
 
 
 
@@ -122,6 +161,11 @@ std::ostream& operator<<(std::ostream& os, const String& obj)
 {
 	return os << obj.get_str();
 }
+
+//////////////////////////////////////////////////////////////////////////
+////////////// КОНЕЦ ОПРЕДЕЛНИЯ КЛАССА - CLASS DEFINITION END     //////////////
+
+
 #define HOME_WORK
 #define CONSTRUCTORS_CALLING
 
